e5fc7e55f0d367a3a76fd01ab0ffdfb1aabd984b
Ayush-Mahajan <mail4ayushmahajan@gmail.com>
2019-08-22 11:19:17 +0530

    update

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'ngo-dashboard', loadChildren: './modules/ngo/ngo-dashboard/ngo-dashboard.module#NgoDashboardModule'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
